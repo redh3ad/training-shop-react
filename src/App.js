77876d7e31fabff539df070d5752eea1518171fb
Egor Matyas <play.filin@gmail.com>
2022-02-16 01:39:39 +0300

    feat: change specification of the project and base structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Index from './components/Index/Index';
+import Shop from './components/Shop/Shop';
+import Cart from './components/Cart/Cart';
+import Logout from './components/Logout/Logout';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+      <div className="app__wrapper">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/auth" element={<Logout />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import st from './Navbar.module.scss'
 
 const Navbar = () => {
   return (
-    <div className={st.title}>Navbar</div>
-  )
+    <aside className={st.aside__container}>
+      <Link to="/">Home</Link>
+      <Link to="/shop">Shop</Link>
+      <Link to="/cart">Cart</Link>
+      <Link to="/auth">Logout</Link>
+    </aside>
+  ) 
 }
 
 export default Navbar
